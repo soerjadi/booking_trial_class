21e85bf603255a30843374306f5a9f87bf72d0e6
soerja <soerja.610@gmail.com>
2026-09-09 09:19:42 +0700

    feat(student): update domain models for student requests

--- internal/core/domain/student.go
+++ internal/core/domain/student.go
@@ -5,3 +5,8 @@ type Student struct {
 	Name     string `json:"name"`
 	ParentID int64  `json:"parent_id"`
 }
+
+type CreateStudentRequest struct {
+	Name     string `json:"name" validate:"required"`
+	ParentID int64  `json:"parent_id" validate:"required"`
+}
