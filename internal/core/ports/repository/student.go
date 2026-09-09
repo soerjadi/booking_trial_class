c8376c2a79394527a34458a0909982823f532b44
soerja <soerja.610@gmail.com>
2026-09-09 09:19:55 +0700

    fix: remove update students

--- internal/core/ports/repository/student.go
+++ internal/core/ports/repository/student.go
@@ -10,5 +10,4 @@ import (
 type StudentRepositoryInterface interface {
 	Create(ctx context.Context, request domain.Student) (domain.Student, error)
 	GetByID(ctx context.Context, request int64) (domain.Student, error)
-	Update(ctx context.Context, request domain.Student) error
 }
